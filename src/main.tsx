986fc9573d4a05c32f1efa260b295e2b446695d3
jayluiscano <jeighdesign@gmail.com>
2026-05-28 10:29:14 +0800

    feat: initialize Sentry React SDK in frontend entrypoint with active DSN

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as Sentry from "@sentry/react";
 import './index.css'
 import App from './App.tsx'
+
+Sentry.init({
+  dsn: "https://96261c9c702585ceadf92161155c759e0a4511465151725568.ingest.us.sentry.io/4511465158686848",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
